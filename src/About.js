e0a9acc8175237409c49afdbb41e7fa3a9cb2ec7
briancoco <briannguyencoco@gmail.com>
2022-10-03 13:21:17 -0500

    first commit

---
+++ src/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+      <main className='About'>
+          <h2>About</h2>
+          <p style={{ marginTop: "1rem" }}>Blog App made by briancoco</p>
+      </main>
+  )
+}
+
+export default About
